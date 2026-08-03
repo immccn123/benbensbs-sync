ea0bd15a8e23b9aeeadf7e9d9abe13944ac7a7f0
Imken Luo <me@imken.moe>
2026-08-03 23:53:33 +0800

    Switch build adapter to @deno/svelte-adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,8 @@
-// import adapter from "@deno/svelte-adapter";
-
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@deno/svelte-adapter";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
 			$hook: "src/hooks",
